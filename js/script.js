2ef13693f915b280364cccf3d467bf9d2183ebf0
Pedro França <pedrofrancadev@gmail.com>
2026-05-03 20:50:08 -0300

    adicionando JS no projeto

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const navBar = document.querySelectorAll('.navbar li a');
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+
+  if (url.includes(href)) {
+    link.classList.add('ativo');
+  }
+}
+
+navBar.forEach(ativarLink);
