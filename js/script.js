9fa4750b10d63d954c881a397375429bcee8ec41
Pedro França <pedrofrancadev@gmail.com>
2026-05-10 10:20:17 -0300

    galeria de imagens

--- js/script.js
+++ js/script.js
@@ -23,3 +23,42 @@ function ativarProduto(paramento) {
 }
 
 paramentos.forEach(ativarProduto);
+
+// Perguntas Frequentes
+
+const perguntas = document.querySelectorAll('.perguntas button');
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute('aria-controls');
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle('ativa');
+  const ativa = resposta.classList.contains('ativa');
+  pergunta.setAttribute('aria-expanded', ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener('click', ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+// Galeria Bicicletas
+
+const galeria = document.querySelectorAll('.bicicleta-imagem img');
+const galeriaContainer = document.querySelector('.bicicleta-imagem');
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia('(min-width: 930px)').matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function galeriaEventos(img) {
+  img.addEventListener('click', trocarImagem);
+}
+
+galeria.forEach(galeriaEventos);
